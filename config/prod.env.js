4fc0c6fc90d763ee277ea52025c9b630d7991988
Herman Leung <leung.hm@gmail.com>
2017-09-20 18:33:11 +0800

    Add built dist/

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,15 @@
+var webpack = require('webpack')
+
 module.exports = {
-  NODE_ENV: '"production"'
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    })
+  ]
 }
+
+// module.exports = {
+//   NODE_ENV: '"production"'
+// }
---
+++ dist/app.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/static/:folder/:filename', function(req, res) {
+  res.sendFile(__dirname + '/static/' + req.params.folder + '/' + req.params.filename);
+});
+
+app.listen(8000);
